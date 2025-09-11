fd5e1c89bf15fc7d743c11eb2afd127a8783bee3
AntoineTorrezSuzano <142870032+AntoineTorrezSuzano@users.noreply.github.com>
2025-09-11 14:35:03 +0200

    Update: correct deploy of docker + change title of html file

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,20 @@ else
     ls -la 
     exit 1
 fi
-echo "Building Docker Image..."
-docker build -t raikomusic:latest .
 
 echo "Stopping/removing old container if exists..."
-docker rm -f raikomusic_container >/dev/null 2>&1 || true
+docker rm -f raikomusics_container >/dev/null 2>&1 || true
+
+echo "Deleting raikomusics image..."
+docker rmi raikomusics
+
+echo "Building New Docker Image..."
+docker build -t raikomusics:latest .
 
 echo "Running container on port 6777..."
-docker run -d --restart unless-stopped -p 6777:80 --name raikomusic_container raikomusic:latest
+docker run -d --restart unless-stopped -p 6777:80 --name raikomusics_container raikomusics:latest
 
 HOSTNAMEIP="$(hostname -I | awk '{print $1}')"
-echo "RaikoMusic website is available at:"
+echo "RaikoMusics website is available at:"
 echo "  http://$HOSTNAMEIP:6777"
 echo "  http://localhost:6777"
