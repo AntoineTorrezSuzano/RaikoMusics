a760efef98953e85a4aa8a5d1cf070a8146ee6fc
AntoineTorrezSuzano <142870032+AntoineTorrezSuzano@users.noreply.github.com>
2025-09-11 15:01:12 +0200

    Upgrade: cleaner deploy.sh file and main.yml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+TAG=$(git rev-parse --short HEAD)
 
 if [ -f Dockerfile ]; then
     echo "Dockerfile found."
@@ -11,17 +12,21 @@ else
     exit 1
 fi
 
-echo "Stopping/removing old container if exists..."
-docker rm -f raikomusics_container >/dev/null 2>&1 || true
+echo "Stopping old container if is running..."
+docker stop raikomusics_container >/dev/nul 2>&1 || true
 
-echo "Deleting raikomusics image..."
-docker rmi raikomusics
+echo "Removing old container if it exist..."
+docker rm raikomusics_container >/dev/null 2>&1 || true
 
 echo "Building New Docker Image..."
-docker build -t raikomusics:latest .
+docker build -t raikomusics:$TAG .
 
 echo "Running container on port 6777..."
-docker run -d --restart unless-stopped -p 6777:80 --name raikomusics_container raikomusics:latest
+docker run -d --restart unless-stopped -p 6777:80 --name raikomusics_container raikomusics:$TAG
+
+echo "Cleaning up dangling images..."
+docker image prune -f
+
 
 HOSTNAMEIP="$(hostname -I | awk '{print $1}')"
 echo "RaikoMusics website is available at:"
