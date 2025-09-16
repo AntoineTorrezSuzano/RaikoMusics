bbba5901571115477b5fa6053c8ed00b67c4a098
AntoineTorrezSuzano <142870032+AntoineTorrezSuzano@users.noreply.github.com>
2025-09-16 16:14:47 +0200

    Fix: API's GET method route's middleware remove

--- RaikoMusicsAPI/src/routes/music.route.js
+++ RaikoMusicsAPI/src/routes/music.route.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 // POST /api/music/upload
 router.post("/upload", handleUpload, musicController.uploadMusic);
-router.get("/get/list", handleUpload, musicController.getMusicList)
+router.get("/get/list", musicController.getMusicList)
 
 module.exports = router;
